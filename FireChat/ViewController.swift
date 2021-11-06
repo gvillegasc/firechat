eb4a54534104ba139522f2d16dc06b0381711776
Gerardo Villegas <villegasgerardo.j@gmail.com>
2021-11-06 14:48:11 -0500

    Add project

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Gerardo Villegas on 6/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
